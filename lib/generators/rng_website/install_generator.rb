aa091e7e13d952dbe7a3fc0769d7ce5efe2be2e2
Christian Finck <christian@finck.at>
2012-03-06 15:09:04 +0100

    implemented generator, also copy simple_form _form template to root app

--- lib/generators/rng_website/install_generator.rb
+++ lib/generators/rng_website/install_generator.rb
@@ -2,6 +2,14 @@ module RngWebsite
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
+
+      def copy_scaffold_template
+        copy_file 'scaffold/_form.html.erb', 'lib/templates/erb/scaffold/_form.html.erb'
+      end
+
+      def copy_bootstrap_css
+        copy_file 'bootstrap.css.sass', 'app/assets/stylesheets/bootstrap_import_and_overwrite.css.sass'
+      end
     end
   end
 end
