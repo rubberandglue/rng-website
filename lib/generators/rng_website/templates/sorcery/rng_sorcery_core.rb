fcf1ab9d5509bf90be6e0c8e7a50f2928c9ed1e3
Christian Finck <christian@finck.at>
2012-03-09 14:06:06 +0100

    set admin default to false

--- lib/generators/rng_website/templates/sorcery/rng_sorcery_core.rb
+++ lib/generators/rng_website/templates/sorcery/rng_sorcery_core.rb
@@ -4,7 +4,7 @@ class RngSorceryCore < ActiveRecord::Migration
       t.string :username,         :null => false  # if you use another field as a username, for example email, you can safely remove this field.
       t.string :crypted_password, :default => nil
       t.string :salt,             :default => nil
-      t.boolean :admin
+      t.boolean :admin,           :default => false
 
       t.timestamps
     end
