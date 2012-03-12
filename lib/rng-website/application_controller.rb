71cc013f67eb0d2d9323ca8857618e516ebd84ed
Christian Finck <christian@finck.at>
2012-03-12 10:06:34 +0100

    added ApplicationController module for always used methods

---
+++ lib/rng-website/application_controller.rb
@@ -0,0 +1,19 @@
+module RngWebsite
+  module ApplicationController
+    def self.included(base)
+      base.class_eval do
+        base.rescue_from ActiveRecord::RecordNotFound, :with => :redirect_to_base
+      end
+    end
+
+    def not_authenticated
+      redirect_to login_url, :alert => t('rng-website.not_authenticated')
+    end
+
+    private
+
+    def redirect_to_base
+      redirect_to root_path
+    end
+  end
+end
--- lib/rng-website/engine.rb
+++ lib/rng-website/engine.rb
@@ -4,6 +4,7 @@ require "sorcery"
 require "bootstrap-sass"
 
 module RngWebsite
+  autoload :ApplicationController, 'rng-website/application_controller'
   class Engine < ::Rails::Engine
     config.generators do |g|
       g.test_framework :rspec, :view_specs => false
