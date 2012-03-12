b281843d020ff960156c47ce15dc0b8b5f861979
Christian Finck <christian@finck.at>
2012-03-12 16:21:53 +0100

    require simple_navigation and the custom renderer

--- lib/rng-website/engine.rb
+++ lib/rng-website/engine.rb
@@ -2,8 +2,10 @@ require "rubygems"
 require "simple_form"
 require "sorcery"
 require "bootstrap-sass"
+require "simple_navigation"
 
 module RngWebsite
+  require "rng-website/bootstrap_simple_nav.rb" if defined?(::SimpleNavigation)
   autoload :ApplicationController, 'rng-website/application_controller'
   class Engine < ::Rails::Engine
     config.generators do |g|
