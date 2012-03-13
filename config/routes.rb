d9b608c66d809a33e7b103b6953d61ffe4ed0d33
Christian Finck <christian@finck.at>
2012-03-13 15:34:19 +0100

    localized routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   post    '/login'  => "session#create"
   get     '/login'  => "session#new",     :as => 'login'
   delete  '/logout' => "session#destroy", :as => 'logout'
-  namespace :admin do
-    get '/' => 'admin#index'
-    resources :users
+
+  localized do
+    namespace :admin do
+      get '/' => 'admin#index'
+      resources :users
+    end
   end
 end
