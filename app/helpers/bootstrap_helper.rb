d8f72ffc1f0752d0de068146f995a01920f8ceb4
Christian Finck <christian@finck.at>
2012-03-23 15:58:13 +0100

    fixed typo

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -5,7 +5,7 @@ module BootstrapHelper
   end
 
   def delete_button_for(url, item)
-    link_to url, class: 'btn btn-mini btn-danger', methode: 'delete', confirm: t('form.delete_msg', item: item) do
+    link_to url, class: 'btn btn-mini btn-danger', method: :delete, confirm: t('form.delete_msg', item: item) do
       "<i class='icon-trash icon-white'></i> ".html_safe +
       t('form.delete')
     end
