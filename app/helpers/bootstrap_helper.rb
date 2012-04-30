abded8789e99c600407d2a82e9b11dfc072f0c6f
Christian Finck <christian@finck.at>
2012-05-01 00:40:56 +0200

    DRYed helper a bit

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -1,21 +1,20 @@
 module BootstrapHelper
   def header(name)
     content_tag :div, content_tag(:h1, name), class: 'page-header'
+  end
 
+  def add_link_to(name, url, options={})
+    icon_link_to name, url, 'plus', options
   end
 
   def delete_button_for(url, item)
-    link_to url, class: 'btn btn-mini btn-danger', method: :delete, confirm: t('form.delete_msg', item: item) do
-      "<i class='icon-trash icon-white'></i> ".html_safe +
-      t('form.delete')
-    end
+    icon_button_to t('form.delete'), url, 'trash icon-white', class: 'btn-danger', method: :delete, confirm: t('form.delete_msg', item: item)
   end
 
-  def back_button_to(url)
-    link_to url, class: 'btn' do
-      "<i class='icon-arrow-left'></i> ".html_safe +
-      t('form.back')
-    end
+  alias :delete_button_to :delete_button_for
+
+  def back_button_to(url, options={})
+    icon_button_to t('form.back'), url, 'arrow-left', options
   end
 
   def footer_back_button_to(url)
@@ -24,18 +23,27 @@ module BootstrapHelper
     end
   end
 
-  def edit_link_to(name, url)
-    link_to url do
-      "<i class='icon-edit'></i> ".html_safe +
-      name
-    end
+  def edit_link_to(name, url, options={})
+    icon_link_to name, url, 'edit', options
   end
 
-  def edit_button_to(name, url)
-    link_to url, class: 'btn btn-mini' do
-      "<i class='icon-edit'></i> ".html_safe +
-      t('form.edit')
-    end
+  def edit_button_to(name, url, options={})
+    icon_button_to name, url, 'edit', options
   end
 
+  def back_link_to(url)
+    icon_link_to t("form.back"), url, 'arrow-left'
+  end
+
+  def icon_button_to(name, url, icon, options={})
+    options[:class] = "btn #{options[:class]}"
+    icon_link_to name, url, icon, options
+  end
+
+  def icon_link_to(name, url, icon, options={})
+    link_to url, options do
+      "<i class='icon-#{icon}'></i> ".html_safe +
+              name
+    end
+  end
 end
